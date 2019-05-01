0ed397677f659d58c385e9996228d679be087702
Erez Friemagor <erezfree29@gmail.com>
2019-05-01 22:45:50 +0100

    config.assets.initialize_on_precompile = false

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,5 @@
 require_relative 'boot'
-config.assets.initialize_on_precompile = false
+
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
@@ -15,6 +15,7 @@ module FindAHobbyBudyy
         end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
