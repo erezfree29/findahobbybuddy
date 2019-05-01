217b7bc53683027b61465449927c4a106c9cd746
Erez Friemagor <erezfree29@gmail.com>
2019-05-01 22:33:07 +0100

    config.assets.initialize_on_precompile = false

--- config/initializers/array_paginate.rb
+++
@@ -1,6 +0,0 @@
-require 'will_paginate/array'
-> numbers = (1..1000).to_a
-> total_pages = numbers.paginate(:page => 2, :per_page => 10)
-=> [11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-> page.total_entries
-=> 1000
